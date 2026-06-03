853f90b1877778b01a0617b88dbe960152027e77
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2026-06-02 21:43:01 -0300

    feat: add lap telemetry DTOs

---
+++ src/CompanionAgent.Core/LapTelemetryDto.cs
@@ -0,0 +1,36 @@
+using System.Text.Json.Serialization;
+
+namespace CompanionAgent.Core;
+
+public class LapTelemetryDto
+{
+    [JsonPropertyName("user_id")]
+    public required string UserId { get; init; }
+
+    [JsonPropertyName("session_source_id")]
+    public required string SessionSourceId { get; init; }
+
+    [JsonPropertyName("lap_number")]
+    public int LapNumber { get; init; }
+
+    [JsonPropertyName("data")]
+    public required LapTelemetryDataDto Data { get; init; }
+
+    [JsonPropertyName("sample_hz")]
+    public int SampleHz { get; init; } = 20;
+}
+
+public class LapTelemetryDataDto
+{
+    [JsonPropertyName("p")]
+    public required int[][] Points { get; init; }
+
+    [JsonPropertyName("s")]
+    public required float[] SectorBoundaries { get; init; }
+
+    [JsonPropertyName("mv")]
+    public int MaxSpeed { get; init; }
+
+    [JsonPropertyName("dur")]
+    public int DurationMs { get; init; }
+}
